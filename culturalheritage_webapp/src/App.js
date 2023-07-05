8c150a5adeae64b8d00a73082a12bfd5bdfab18d
unknown <chchatzidiakos@outlook.com>
2023-07-05 12:45:16 +0300

    change short in the App test from n lap

--- culturalheritage_webapp/src/App.js
+++ culturalheritage_webapp/src/App.js
@@ -2,13 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainMap from "./components/MainMap.js";
 import React from "react";
-import 'leaflet/dist/leaflet.css';
-import 'leaflet/dist/leaflet.js';
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.js";
 
 function App() {
-  return (
-      <MainMap />
-  )
+  return <MainMap />;
 }
 
 export default App;
