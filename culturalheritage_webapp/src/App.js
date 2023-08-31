42294f2236cc3a41ab3fb6c974ed360577547d99
chchatzi1 <chchatzidiakos@outlook.com>
2023-08-31 11:39:49 +0300

    initialize authentication in auth branch

--- culturalheritage_webapp/src/App.js
+++ culturalheritage_webapp/src/App.js
@@ -1,12 +1,28 @@
 import logo from "./logo.svg";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import "./App.css";
 import MainMap from "./components/MainMap.js";
 import React from "react";
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet.js";
+import AuthenticationPage from "./pages/Authentication";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainMap />,
+    children: [
+      {
+        path: '/auth',
+        element: <AuthenticationPage />,
+      }
+    ]
+  },
+]);
 
 function App() {
-  return <MainMap />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
